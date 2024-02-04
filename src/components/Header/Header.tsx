0639b45b3719d7f82c188c95ebf730774584d5cb
Kioku <anhkhoakioku@gmail.com>
2024-02-04 11:22:36 +0700

    Update UI header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
           <Link className='col-span-2' to='/'>
             <img src={logo} alt='logo'></img>
           </Link>
-          <form className='col-span-3 lg:col-span-5 col-start-3'>
+          <form className='col-span-7 lg:col-span-5 col-start-3'>
             <div className='bg-gray-100 rounded-full p-1 flex border-solid'>
               <button className='rounded-sm py-2 px-6 flex-shrink-0'>
                 <svg
@@ -35,50 +35,8 @@ export default function Header() {
               />
             </div>
           </form>
-          <div className='col-span-5 lg:col-start-10 flex justify-center lg:justify-end'>
-            <Popover
-              className='flex items-center px-5 py-1 hover:text-gray-900 cursor-pointer'
-              renderPopover={
-                <div className='relative rounded-sm border border-gray-200 bg-white shadow-md'>
-                  <div className='flex flex-col py-2 pr-28 pl-3'>
-                    <button className='py-2 px-3 text-left hover:text-orange' onClick={() => {}}>
-                      VN
-                    </button>
-                    <button className='mt-2 py-2 px-3 text-left hover:text-orange' onClick={() => {}}>
-                      EN
-                    </button>
-                  </div>
-                </div>
-              }
-            >
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                strokeWidth={1.5}
-                stroke='currentColor'
-                className='h-5 w-5'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  d='M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418'
-                />
-              </svg>
-              <span className='mx-1'>VN</span>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                strokeWidth={1.5}
-                stroke='currentColor'
-                className='h-5 w-5'
-              >
-                <path strokeLinecap='round' strokeLinejoin='round' d='M19.5 8.25l-7.5 7.5-7.5-7.5' />
-              </svg>
-            </Popover>
-            <div className='flex items-center py-1 hover:text-gray-900 cursor-pointer'></div>
-            <div className='flex items-center py-1 hover:text-gray-900 cursor-pointer'>
+          <div className='col-span-10 lg:col-span-5 lg:col-start-10 grid grid-cols-5 justify-center items-center'>
+            <div className='col-span-2 py-1 flex gap-2 hover:text-gray-900 cursor-pointer'>
               <svg
                 xmlns='http://www.w3.org/2000/svg'
                 fill='none'
@@ -95,7 +53,7 @@ export default function Header() {
               </svg>
               <span>Write</span>
             </div>
-            <div className='flex items-center py-1 hover:text-gray-900 cursor-pointer ml-6'>
+            <div className='col-span-1 col-start-3 py-1 pl-2 hover:text-gray-900 cursor-pointer'>
               <svg
                 xmlns='http://www.w3.org/2000/svg'
                 fill='none'
@@ -111,16 +69,93 @@ export default function Header() {
                 />
               </svg>
             </div>
-            <div className='flex items-center py-1 hover:text-gray-900 cursor-pointer ml-6'>
-              <div className='w-5 h-5 mr-2 flex-shrink-0'>
-                <img
-                  src='https://miro.medium.com/v2/resize:fill:64:64/0*4VeCVLdrzYDI6Ki6'
-                  alt='avatar'
-                  className='w-full h-full object-cover rounded-full'
-                ></img>
+            <Popover
+              className='flex items-center py-1 hover:text-gray-900 cursor-pointer ml-6'
+              renderPopover={
+                <div className='drop-shadow-md'>
+                  <Link
+                    to='/'
+                    className='block w-full bg-white py-3 px-4 text-left hover:bg-slate-100 hover:text-cyan-500'
+                  >
+                    <div className='flex justify-start pr-28 gap-2'>
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        fill='none'
+                        viewBox='0 0 24 24'
+                        strokeWidth='1.5'
+                        stroke='currentColor'
+                        className='w-5 h-5'
+                      >
+                        <path
+                          strokeLinecap='round'
+                          strokeLinejoin='round'
+                          d='M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z'
+                        />
+                      </svg>
+                      <span className='mx-1'>Profile</span>
+                    </div>
+                  </Link>
+                  <Link
+                    to='/'
+                    className='block w-full bg-white py-3 px-4 text-left hover:bg-slate-100 hover:text-cyan-500'
+                  >
+                    <div className='flex justify-start pr-28 gap-2'>
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        fill='none'
+                        viewBox='0 0 24 24'
+                        strokeWidth='1.5'
+                        stroke='currentColor'
+                        className='w-5 h-5'
+                      >
+                        <path
+                          strokeLinecap='round'
+                          strokeLinejoin='round'
+                          d='M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z'
+                        />
+                      </svg>
+                      <span className='mx-1'>Library</span>
+                    </div>
+                  </Link>
+                  <Link
+                    to='/'
+                    className='block w-full bg-white py-3 px-4 text-left hover:bg-slate-100 hover:text-cyan-500'
+                  >
+                    <div className='flex justify-start pr-28 gap-2'>
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        fill='none'
+                        viewBox='0 0 24 24'
+                        strokeWidth='1.5'
+                        stroke='currentColor'
+                        className='w-5 h-5'
+                      >
+                        <path
+                          strokeLinecap='round'
+                          strokeLinejoin='round'
+                          d='M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z'
+                        />
+                      </svg>
+                      <span className='mx-1'>Stories</span>
+                    </div>
+                  </Link>
+                  <button className='block w-full bg-white py-6 px-4 text-left hover:bg-slate-100 hover:text-cyan-500 border-t'>
+                    Log out
+                  </button>
+                </div>
+              }
+            >
+              <div className='col-span-2 col-start-4 py-1 flex gap-2 hover:text-gray-900 cursor-pointer'>
+                <div className='w-5 h-5 mr-2 flex-shrink-0'>
+                  <img
+                    src='https://miro.medium.com/v2/resize:fill:64:64/0*4VeCVLdrzYDI6Ki6'
+                    alt='avatar'
+                    className='w-full h-full object-cover rounded-full'
+                  ></img>
+                </div>
+                <div>Kioku</div>
               </div>
-              <div>Kioku</div>
-            </div>
+            </Popover>
           </div>
         </div>
       </div>
