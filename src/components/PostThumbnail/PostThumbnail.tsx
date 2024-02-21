ea7d0ca9ee113d544fad4c56fcc580062f29d983
Kioku <anhkhoakioku@gmail.com>
2024-02-21 16:24:08 +0700

    Add UI PostThumnail component

---
+++ src/components/PostThumbnail/PostThumbnail.tsx
@@ -0,0 +1,36 @@
+import thumbnail from 'src/assets/images/demo-thumbnail.jpeg'
+import avatarIcon from 'src/assets/images/demo-avatar.jpeg'
+
+export default function PostThumbnail() {
+  return (
+    <div className='relative h-96'>
+      <div
+        className='absolute top-0 left-0 h-full w-full bg-cover bg-center flex items-center'
+        style={{ backgroundImage: `url(${thumbnail})` }}
+      ></div>
+      <div className='absolute top-0 left-0 h-full w-full bg-black opacity-40'></div>
+      <div className='absolute top-0 left-0 h-full w-full flex items-center'>
+        <div className='container flex flex-col items-center'>
+          <button className='rounded-full my-4 py-2 px-4 bg-black border-2 border-white'>
+            <div className='text-base font-medium text-white cursor-pointer hover:text-lightBlue'>Data Source</div>
+          </button>
+          <div className='my-4 text-3xl font-extrabold text-white cursor-pointer'>
+            Reinforcement Learning 101: Building a RL Agent
+          </div>
+          <div className='flex justify-around items-center gap-2 cursor-pointer'>
+            <img
+              src={avatarIcon}
+              alt='avatar-icon'
+              className='w-8 h-8 rounded-full border border-lightBlue cursor-pointer'
+            ></img>
+            <div className='text-xl font-thin text-white hover:text-lightBlue hover:underline'>Kioku</div>
+            <div className='text-xl font-thin text-white'>/</div>
+            <div className='text-xl font-thin text-white'>January 11 2024</div>
+            <div className='text-xl font-thin text-white'>/</div>
+            <div className='text-xl font-thin text-white'>Data Source</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
