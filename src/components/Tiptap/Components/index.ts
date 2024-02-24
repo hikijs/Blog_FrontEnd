8bace3a6c41a0b92f8f18aaddece996f95421c79
Kioku <anhkhoakioku@gmail.com>
2024-02-24 12:01:15 +0700

    Add TipTap Component
    - Install Tiptap packages for handling rich text edior
    - Create Tiptap Component depand on Novel source 'https://github.com/steven-tey/novel'

---
+++ src/components/Tiptap/Components/index.ts
@@ -0,0 +1,9 @@
+export { useCurrentEditor as useEditor } from '@tiptap/react'
+export { type Editor } from '@tiptap/core'
+export type { JSONContent } from '@tiptap/react'
+
+export { EditorRoot, EditorContent, type EditorContentProps, defaultEditorProps } from './Editor'
+export { EditorBubble } from './EditorBubble'
+export { EditorBubbleItem } from './EditorBubbleItem'
+export { EditorCommand } from './EditorCommand'
+export { EditorCommandItem, EditorCommandEmpty } from './EditorCommandItem'
--- tailwind.config.js
+++ tailwind.config.js
@@ -32,6 +32,7 @@ module.exports = {
         }
       })
     }),
-    require('@tailwindcss/line-clamp')
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/typography')
   ]
 }
